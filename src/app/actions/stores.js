43a1f4d80456f026db648d9c0e8f8e3e44e37485
jorge <jorge.delucca@geofusion.com.br>
2019-10-08 16:08:57 -0300

    serverless mode

--- src/app/actions/stores.js
+++ src/app/actions/stores.js
@@ -1,21 +1,19 @@
-import httpClient from '../httpClient';
 import { STORES_FETCHED } from '../actionTypes';
+import * as data from '../../data.json';
 
 function fetchStores() {
     return async dispatch => {
-        const url = '/stores/';
-        const { data } = await httpClient.get(url);
+        // const { data } = await httpClient.get(url);
         // I'm bindinding index as an id into each item in order to control them later with leaflet markers
-        const stores = data.map((store, i) => ({ ...store, id: i }));
+        const stores = data.stores.map((store, i) => ({ ...store, id: i }));
         dispatch({ stores, type: STORES_FETCHED });
     };
 }
 
 function updateStores() {
     return async dispatch => {
-        const url = '/stores';
-        const { data } = await httpClient.get(url);
-        dispatch({ stores: data, type: STORES_FETCHED });
+        // const { data } = await httpClient.get(url);
+        dispatch({ stores: data.stores, type: STORES_FETCHED });
     };
 }
 
